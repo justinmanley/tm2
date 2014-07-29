ab74f69fa95b723e044f7dcdaa52f720ac78e676
Young Hahn <young@mapbox.com>
2014-07-29 16:00:45 -0400

    What it takes to atom, minus all the build issues.

--- index.js
+++ index.js
@@ -590,3 +590,37 @@ app.listen(config.port, function(err) {
     console.log('TM2 @ http://localhost:'+config.port+'/');
 });
 
+var atom;
+try { atom = require('app'); } catch(err) {}
+if (atom) {
+    var BrowserWindow = require('browser-window');  // Module to create native browser window.
+
+    // Report crashes to our server.
+    require('crash-reporter').start();
+
+    // Keep a global reference of the window object, if you don't, the window will
+    // be closed automatically when the javascript object is GCed.
+    var mainWindow = null;
+
+    // Quit when all windows are closed.
+    atom.on('window-all-closed', function() { atom.quit(); });
+
+    // This method will be called when atom-shell has done everything
+    // initialization and ready for creating browser windows.
+    atom.on('ready', function() {
+        // Create the browser window.
+        mainWindow = new BrowserWindow({width: 800, height: 600});
+
+        // and load the index.html of the app.
+        mainWindow.loadUrl('http://localhost:3000');
+
+        // Emitted when the window is closed.
+        mainWindow.on('closed', function() {
+            // Dereference the window object, usually you would store windows
+            // in an array if your app supports multi windows, this is the time
+            // when you should delete the corresponding element.
+            mainWindow = null;
+        });
+    });
+}
+
